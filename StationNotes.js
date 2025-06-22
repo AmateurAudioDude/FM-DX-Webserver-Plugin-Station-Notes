b520093997fd01a93052121ebe133424f2c9081b
Amateur Audio Dude <168192910+AmateurAudioDude@users.noreply.github.com>
2025-06-23 00:55:45 +1000

    v1.0.1

--- StationNotes.js
+++ StationNotes.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Station Notes',
-    version: '1.0.0',
+    version: '1.0.1',
     author: 'AAD',
     frontEndPath: 'StationNotes/pluginStationNotes.js'
 }
